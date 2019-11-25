478d76b055fc249cda1d7536921fdc6e6efc56dc
github username <github email address>
2019-11-25 15:49:39 -0500

    l

--- tools/console.rb
+++ tools/console.rb
@@ -12,10 +12,14 @@ c2 = Cult.new("Cult of John Jones", "Jonestown", 1978, "Drink the koolaid!")
 c3 = Cult.new("Heaven's Gate", "California", 1974, "Let's go to Heaven!")
 c4 = Cult.new("Hell's Gate", "California", 1975, "Let's go to Hell!")
 
+
 f1 = Follower.new("Johnny", 21, "YOLO")
 f2 = Follower.new("Stanley", 20, "I'm too young for this ish")
 f3 = Follower.new("Jago", 25, "Combo Breaker!")
 
+o1 = f1.join_cult(c3, 2019)
+o2 = f2.join_cult(c2, 2019)
+o3 = f3.join_cult(c4, 2018)
 
 
 binding.pry
